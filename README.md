d32954885f053402e89814a1e21de3c508d070b3
fengberd <admin@berd.moe>
2020-01-20 23:18:12 +0800

    Now you can configure email forwarding! Also added a PHP API proxy example.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # NamecheapPanel
 An alternative of namecheap's domain management panel. Their system is way too laggy.
 
+### A PHP API proxy example
+```PHP
+<?php
+header('Access-Control-Allow-Origin: *');
+curl_setopt_array($ch=curl_init(),array(
+	CURLOPT_URL=>'https://api.'.(isset($_GET['sandbox'])?'sandbox.':'').'namecheap.com/xml.response',
+	CURLOPT_POST=>true,
+	CURLOPT_HEADER=>false,
+	CURLOPT_TIMEOUT=>10,
+	CURLOPT_POSTFIELDS=>http_build_query(array_merge($_POST,array(
+		'ClientIP'=>$_SERVER['REMOTE_ADDR']))),
+	CURLOPT_HTTPHEADER=>$headers,
+	CURLOPT_AUTOREFERER=>true,
+	CURLOPT_FOLLOWLOCATION=>true,
+	CURLOPT_RETURNTRANSFER=>true));
+echo(curl_exec($ch));
+curl_close($ch);
+
+```
+
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- src/views/DomainManage.vue
+++ src/views/DomainManage.vue
@@ -5,7 +5,7 @@
 				<h2>DNS Records</h2>
 			</v-flex>
 			<v-flex xs12>
-				<v-data-table :headers="headers" :items="records" :loading="loading" :no-data-text="error==null?'Loading...':error" :rows-per-page-items="[-1]" disable-initial-sort class="elevation-2">
+				<v-data-table :headers="recordHeaders" :items="records" :loading="loading" :no-data-text="error==null?'Loading...':error" :rows-per-page-items="[-1]" disable-initial-sort class="elevation-2">
 					<template v-slot:items="s">
 						<td>
 							<v-edit-dialog :return-value="s.item.type" lazy>
@@ -16,16 +16,16 @@
 							</v-edit-dialog>
 						</td>
 						<td>
-							<v-edit-dialog :return-value="s.item.name" @open="if(s.item.host=='Click Me') s.item.host=''" lazy>
-								{{ s.item.host }}
+							<v-edit-dialog :return-value="s.item.name" lazy>
+								{{ s.item.host || 'Click Me' }}
 								<template v-slot:input>
 									<v-text-field @change="changed = true" v-model="s.item.host" label="Host" single-line></v-text-field>
 								</template>
 							</v-edit-dialog>
 						</td>
 						<td>
-							<v-edit-dialog :return-value="s.item.value" @open="if(s.item.value=='Click Me') s.item.value=''" lazy>
-								{{ s.item.value }}
+							<v-edit-dialog :return-value="s.item.value" lazy>
+								{{ s.item.value || 'Click Me' }}
 								<template v-slot:input>
 									<!-- // TODO: Add a select box for CAA record -->
 									<v-text-field @change="changed = true" v-model="s.item.value" label="Value" single-line></v-text-field>
@@ -57,10 +57,41 @@
 				<h2>Mail Settings</h2>
 			</v-flex>
 			<v-flex xs12>
-				<v-select :items="emailTypes" v-model="emailType" item-value="key" item-text="name" @change="changed = true" label="Mail DNS Mode" class="mb-2" hide-details solo></v-select>
+				<v-select :items="emailTypes" v-model="emailType" item-value="key" item-text="name" @change="changed = true" label="DNS Mode" hide-details solo></v-select>
 			</v-flex>
-			<v-flex xs12 style="text-align: end;">
-				<v-btn class="mr-0 mt-2" :disabled="!changed" @click="saveRecords()">SAVE</v-btn>
+			<v-flex xs12 style="text-align: end;" class="my-2">
+				<v-btn class="mr-2" :disabled="!forwardChanged" @click="saveForwards()" v-if="this.emailType=='FWD'">↓ SAVE</v-btn>
+				<v-btn class="mr-0" :disabled="!changed" @click="saveRecords()">SAVE ↑</v-btn>
+			</v-flex>
+			<v-flex xs12>
+				<v-data-table :headers="forwardHeaders" :items="forwards" :loading="loading" :no-data-text="error==null?'Loading...':error" :rows-per-page-items="[-1]" disable-initial-sort class="elevation-2" v-if="this.emailType=='FWD'">
+					<template v-slot:items="s">
+						<td>
+							<v-edit-dialog :return-value="s.item.mailbox" lazy>
+								{{ s.item.mailbox || 'Click Me' }}
+								<template v-slot:input>
+									<v-text-field @change="forwardChanged = true" v-model="s.item.mailbox" label="MailBox, * for catch-all" single-line></v-text-field>
+								</template>
+							</v-edit-dialog>
+						</td>
+						<td>
+							<v-edit-dialog :return-value="s.item.target" lazy>
+								{{ s.item.target || 'Click Me' }}
+								<template v-slot:input>
+									<v-text-field @change="forwardChanged = true" v-model="s.item.target" label="Target" single-line></v-text-field>
+								</template>
+							</v-edit-dialog>
+						</td>
+						<td>
+							<v-btn flat icon @click="deleteForward(s.item);">
+								<v-icon>delete</v-icon>
+							</v-btn>
+						</td>
+					</template>
+					<template v-slot:actions-append>
+						<v-btn flat color="white" @click="newForward()">NEW</v-btn>
+					</template>
+				</v-data-table>
 			</v-flex>
 		</v-layout>
 	</v-container>
@@ -75,13 +106,6 @@ export default
 	},
 	data: ()=>(
 	{
-		headers: [
-			{ text: 'Type', value: 'type' },
-			{ text: 'Host', value: 'host' },
-			{ text: 'Value', value: 'value' },
-			{ text: 'TTL', value: 'ttl' },
-			{ text: 'Action', sortable: false }
-		],
 		domainTypes: [
 			'A',
 			'AAAA',
@@ -111,12 +135,30 @@ export default
 			{ key: 'OX', name: 'Private Email' },
 			{ key: 'GMAIL', name: 'Gmail' }
 		],
+		
 		domain: '',
 		sld: '',
 		tld: '',
-		records: [],
 		emailType: '',
+		
+		records: [],
+		recordHeaders: [
+			{ text: 'Type', value: 'type' },
+			{ text: 'Host', value: 'host' },
+			{ text: 'Value', value: 'value' },
+			{ text: 'TTL', value: 'ttl' },
+			{ text: 'Action', sortable: false }
+		],
 		changed: false,
+		
+		forwards: [],
+		forwardHeaders: [
+			{ text: 'MailBox', value: 'mailbox' },
+			{ text: 'Target', value: 'target' },
+			{ text: 'Action', sortable: false }
+		],
+		forwardChanged: false,
+		
 		loading: false,
 		error: null
 	}),
@@ -134,6 +176,7 @@ export default
 			this.sld=d.shift();
 			this.tld=d.join('.');
 			this.records=[];
+			this.emailType='';
 			this.changed=false;
 			this.loading=true;
 			this.$root.fetchAPI('namecheap.domains.dns.getHosts',
@@ -143,7 +186,7 @@ export default
 			}).then(data=>
 			{
 				data=data.querySelector('DomainDNSGetHostsResult');
-				if(data.attributes.Domain.value!=this.sld+'.'+this.tld)
+				if(data.attributes.Domain.value!=this.domain)
 				{
 					return;
 				}
@@ -169,18 +212,19 @@ export default
 					this.records.push(e);
 				});
 				this.loading=false;
+				this.reloadForward();
 			});
 		},
 		newRecord()
 		{
 			this.records.push(
 			{
-				id: -1,
+				// Don't set id, it's not used actually
 				ddns: false,
 				ttl: '300',
 				type: 'A',
-				host: 'Click Me',
-				value: 'Click Me',
+				host: '',
+				value: '',
 				mx_preference: '10'
 			});
 		},
@@ -190,20 +234,17 @@ export default
 			if(index!==-1)
 			{
 				this.records.splice(index,1);
-			}
-			else
-			{
-				// TODO: Alert user
-				console.error('WTF');
+				this.changed=true;
 			}
 		},
 		saveRecords()
 		{
+			this.loading=true;
 			var data=
 			{
 				SLD: this.sld,
 				TLD: this.tld,
-				EmailType: this.emailType // TODO
+				EmailType: this.emailType
 			};
 			for(var i=1;i<=this.records.length;i++)
 			{
@@ -217,10 +258,101 @@ export default
 				data['TTL'+i]=rec.ttl;
 			}
 			this.$root.fetchAPI('namecheap.domains.dns.setHosts',data)
+				.then(data=>
+				{
+					this.emailType=data.querySelector('DomainDNSSetHostsResult').attributes.EmailType.value;
+					
+					// TODO: Alert user
+					console.info(data);
+					
+					this.loading=false;
+					// TODO: Temporary solution for illegal updates that should be verified here
+					this.reload();
+				})
+				.catch(()=>
+				{
+					this.loading=false;
+				});
+		},
+		reloadForward()
+		{
+			if(this.emailType!='FWD')
+			{
+				return;
+			}
+			this.loading=true;
+			this.forwards=[];
+			this.$root.fetchAPI('namecheap.domains.dns.getEmailForwarding',
+			{
+				DomainName: this.domain
+			})
 			.then(data=>
 			{
-				console.info(data);
+				data=data.querySelector('DomainDNSGetEmailForwardingResult');
+				if(data.attributes.Domain.value!=this.domain)
+				{
+					return;
+				}
+				data.querySelectorAll('Forward').forEach(e=>
+				{
+					this.forwards.push(
+					{
+						id: e.attributes.mailboxid.value,
+						mailbox: e.attributes.mailbox.value,
+						target: e.innerHTML
+					});
+				});
+				this.loading=false;
+			})
+			.catch(()=>
+			{
+				this.loading=false;
 			});
+		},
+		newForward()
+		{
+			this.forwards.push(
+			{
+				// Don't set id, it's not used actually
+				mailbox: '',
+				target: ''
+			});
+		},
+		deleteForward(forward)
+		{
+			var index=this.forwards.indexOf(forward);
+			if(index!==-1)
+			{
+				this.forwards.splice(index,1);
+				this.forwardChanged=true;
+			}
+		},
+		saveForwards()
+		{
+			this.loading=true;
+			var data=
+			{
+				DomainName: this.domain
+			};
+			for(var i=1;i<=this.forwards.length;i++)
+			{
+				var fwd=this.forwards[i-1];
+				data['MailBox'+i]=fwd.mailbox;
+				data['ForwardTo'+i]=fwd.target;
+			}
+			this.$root.fetchAPI('namecheap.domains.dns.setEmailForwarding',data)
+				.then(data=>
+				{
+					// TODO: Alert user
+					console.info(data);
+					
+					this.loading=false;
+					this.reloadForward();
+				})
+				.catch(()=>
+				{
+					this.loading=false;
+				});
 		}
 	}
 }
